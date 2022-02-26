de9ba99084b847fc777ac6dc37e9e0bacf253e52
TaeGyun12 <tg1218.kim@gmail.com>
2022-02-26 18:22:54 +0900

    [수정] 1. daemon 삭제 2. config 파일 수정

--- back/connector.js
+++ back/connector.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const url = require('../daemon/config/stack_config');
+const url = require('./config/stacks_config');
 
 
 function getURL(chain) {
--- daemon/config/stack_config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HTTPS_MAINNET_URL: "https://stacks-node-api.mainnet.stacks.co/extended/v1/",
-    HTTPS_TESTNET_URL: "https://stacks-node-api.testnet.stacks.co/extended/v1/",
-    WSS_MAINET_URL: "wss://stacks-node-api.mainnet.stacks.co/",
-    WSS_TESTNET_URL: "wss://stacks-node-api.testnet.stacks.co/"    
-};
